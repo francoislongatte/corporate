660bc44df56d82625fc8fa24fc55529cbe8b6a33
francois <francois.longatte@gmail.com>
2023-11-20 13:28:20 +0100

    add logo

---
+++ src/app/core/logo/logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+	selector: '.logo',
+	standalone: true,
+	imports: [],
+	templateUrl: './logo.component.html'
+})
+export class LogoComponent {}
--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { LogoComponent } from '../logo/logo.component'
 
 @Component({
-  selector: '[Navbar]',
-  templateUrl: './navbar.component.html',
-  standalone: true,
-  imports: [],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: '[Navbar]',
+	templateUrl: './navbar.component.html',
+	standalone: true,
+	imports: [LogoComponent],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent {}
